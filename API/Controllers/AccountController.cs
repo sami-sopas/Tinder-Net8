f989aca7f4d6dd9abe77264726318ecd1a435be1
sami-sopas <ramirezcuenca@outlook.es>
2024-08-15 10:11:17 -0600

    creating a UserDTO and returning the token

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,15 +4,16 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(DataContext context) : BaseApiController
+public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
 {
     [HttpPost("register")] // api/account/register
-    public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDto)
+    public async Task<ActionResult<UserDTO>> Register(RegisterDTO registerDto)
     {
         if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
@@ -29,10 +30,15 @@ public class AccountController(DataContext context) : BaseApiController
         context.Users.Add(user);
         await context.SaveChangesAsync();
 
-        return user;
+        return new UserDTO
+        {
+            Username = user.UserName,
+            Token = tokenService.CreateToken(user)
+        };
     }
+
     [HttpPost("login")]
-    public async Task<ActionResult<AppUser>> Login(LoginDTO loginDto)
+    public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDto)
     {
         var user = await context.Users.FirstOrDefaultAsync(x => 
             x.UserName == loginDto.Username.ToLower());
@@ -48,7 +54,11 @@ public class AccountController(DataContext context) : BaseApiController
             if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
         }
 
-        return user;
+        return new UserDTO
+        {
+            Username = user.UserName,
+            Token = tokenService.CreateToken(user)
+        };
     }
 
     private async Task<bool> UserExists(string username)
