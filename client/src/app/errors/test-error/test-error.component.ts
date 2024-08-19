1b0dcc07a13475ec31f8071f66ed5c39af76dd74
Samuel Cuenca <ramirezcuenca@outlook.es>
2024-08-18 18:45:03 -0600

    testing errors in the client

---
+++ client/src/app/errors/test-error/test-error.component.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.css']
+})
+export class TestErrorComponent implements OnInit {
+
+  baseUrl = 'http://localhost:5000/api/';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  get404Error() {
+    this.http.get(this.baseUrl + 'buggy/not-found').subscribe({
+      error: response => console.log(response),
+    })
+  }
+
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/bad-request').subscribe({
+      error: response => console.log(response),
+    })
+  }
+
+
+  get500Error() {
+    this.http.get(this.baseUrl + 'buggy/server-error').subscribe({
+      error: response => console.log(response),
+    })
+  }
+
+
+  get401Error() {
+    this.http.get(this.baseUrl + 'buggy/auth').subscribe({
+      error: response => console.log(response),
+    })
+  }
+
+  get400ValidationError() {
+    this.http.post(this.baseUrl + 'account/register', {}).subscribe({
+      next: response => console.log(response),
+      error: response => console.log(response),
+    })
+  }
+
+
+}
