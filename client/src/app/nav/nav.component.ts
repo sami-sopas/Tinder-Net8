41c92e6cc6b50e178e0f8c0e46b2964f0350b048
Samuel Cuenca <ramirezcuenca@outlook.es>
2024-08-18 20:34:22 -0600

    handling not found and server error page

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -42,7 +42,7 @@ export class NavComponent implements OnInit {
       },
       error: error => {
         console.log(error);
-        this.toastr.error(error.error);
+        //this.toastr.error(error.error);
       }
     })
   }
