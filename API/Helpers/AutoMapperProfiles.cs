110c474bb4bfe8ab6d98d03ac1bf37a7fb25d0f5
sami-sopas <ramirezcuenca@outlook.es>
2024-08-22 21:16:42 -0600

    specific prop with automapper

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -9,7 +9,11 @@ namespace API.Helpers
         public AutoMapperProfiles()
         {
             //Convert AppUser to MemberDTO
-            CreateMap<AppUser, MemberDTO>();
+            CreateMap<AppUser, MemberDTO>()
+                //Para realizar alguna accion de un campo, se usa ForMember
+                .ForMember(
+                    dest => dest.PhotoUrl, //Destino: MemberDTO.PhotoUrl
+                    opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url)); //Origen: AppUser.Photos.Url (donde IsMain == true)
 
             //Convert Photo to PhotoDTO
             CreateMap<Photo, PhotoDTO>();
